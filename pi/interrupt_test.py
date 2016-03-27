d4af31bbdba02aef3433e5b8cbf11c10dc185b62
Michael Bunsen <notbot@gmail.com>
2016-03-27 02:31:09 -0700

    Experimenting with interrupts for a faster counter

---
+++ pi/interrupt_test.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+
+import os
+import sys
+import signal
+import datetime as dt
+
+
+n = 0
+starttime = dt.datetime.now()
+
+def show_results(*args, **kwargs):
+        # print(args, kwargs)
+        result = "{:,d}".format(n)
+        elapsed_time = dt.datetime.now() - starttime
+        print("\n\nCounted to: {}".format(result))
+        print("In {:,.2f} seconds".format(
+            elapsed_time.total_seconds()))
+        print("That's {:,.0f} per second".format(
+            n / elapsed_time.total_seconds()))
+
+signal.signal(signal.SIGUSR1, show_results)
+
+print("PID: {}".format(os.getpid()))
+print("Show current count with: `kill -USR1 {}`".format(os.getpid()))
+
+while True:
+    try:
+        n = n + 1
+    except KeyboardInterrupt:
+        show_results()
+        sys.exit(0)
+        # raise
